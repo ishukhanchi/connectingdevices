1c38f32b670189f295d700dec6aeefd37bb2e3ed
ishu khanchi <ishukhanchi@gmail.com>
2018-07-06 15:07:34 +0530

    Urls are added.

--- app1/models.py
+++ app1/models.py
@@ -1,8 +1,7 @@
 from django.db import models
 
 # Create your models here.
-class  Device():
- 	pass Device(models.Model):
-	id = models.IntegerField(max_length=20)
-    api = models.CharField(max_length=200)
-    status = models.CharField(max_length=200)
+class  Device(models.Model):
+	id = models.IntegerField(primary_key='true')
+	api = models.CharField(max_length=200)
+	status = models.CharField(max_length=200)
--- project01/settings.py
+++ project01/settings.py
@@ -31,12 +31,14 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'app1.apps.App1Config',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
