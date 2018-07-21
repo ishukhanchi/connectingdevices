955e861310be54a5907b3e140ade92a0f31ecf43
ishu khanchi <ishukhanchi@gmail.com>
2018-07-21 16:49:12 +0530

    Basic html pages are added.

--- app1/views.py
+++ app1/views.py
@@ -1,5 +1,30 @@
 from django.http import HttpResponse
-# Create your views here.
+from django.shortcuts import render
+from django.core.context_precessors import csrf
+from django.contrib.auth import authenticate, login
 
-def index(request):
-	return HttpResponse("hello");
+def login(request):
+	c = {}
+	c.update(csrf(request))
+	return render_to_response('login.html', c)
+
+def my_view(request):
+    username = request.POST['username']
+    password = request.POST['password']
+    user = authenticate(request, username=username, password=password)
+    if user is not None:
+        login(request, user)
+        return render(request, '/templates/home.html', {'app1': p})
+    else:
+        return HttpResponse("invalid user")
+
+def loggedin(request):
+	return render_to_response(loggedin.html.
+		                      {'full_name': request.user.usernmae})
+
+def invalid_login(request):
+	return render_to_response('invalid_login.html')
+
+def logout(request):
+	auth.logout(request)
+	return render_to_response('logout.html')
